d5c105b894d8be1439a799595b11fd14c15ba9d1
Dénes Harmath <harmathdenes@gmail.com>
2026-07-30 02:47:56 +0200

    Typecheck on every change

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    checker({
+      typescript: {
+        tsconfigPath: "./tsconfig.app.json",
+      },
+    }),
+  ],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
